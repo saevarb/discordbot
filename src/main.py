625b0f22dd49002614ea1b09053a278327f7f9fa
Sævar Berg <icedane@gmail.com>
2017-02-26 14:57:21 +0100

    Fixed insult generation.

--- src/main.py
+++ src/main.py
@@ -25,15 +25,17 @@ async def on_message(message):
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
     if message.content.startswith('!insult'):
         hasTarget = len(message.content.split()) > 1
-
         if hasTarget:
             targets = message.mentions
+            insults = []
+            await client.send_typing(message.channel)
             for target in message.mentions:
-                print('Insulting {}'.format(target.mention))
-                with urllib.request.urlopen('http://insult.mattbas.org/api/insult.txt?who=' + target.name) as f:
+                # print('Insulting {}'.format(target.mention))
+                with urllib.request.urlopen('http://insult.mattbas.org/api/insult.txt?who=' + urllib.parse.quote_plus(target.name)) as f:
                     insult = f.read().decode('utf-8')
                     insult = insult.replace(target.name, target.mention)
-                    await client.send_message(message.channel, insult)
+                    insults.append(insult)
+            await client.send_message(message.channel, '\n'.join(insults))
         else:
             with urllib.request.urlopen('http://insult.mattbas.org/api/insult.txt') as f:
                 insult = f.read().decode('utf-8')
